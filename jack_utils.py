1f02b054226d095a514749c035e18d9d576c4174
Jack B. Greisman <Jack.Greisman@gmail.com>
2015-05-10 01:51:56 -0400

    jack_utils.py: Python function for sending texts

---
+++ jack_utils.py
@@ -0,0 +1,36 @@
+########################################################################
+# jack_utils.py
+# Jack Greisman
+########################################################################
+
+import sys
+
+#----------------------------------------------------------------------#
+# Send a text message to a phone number
+def send_text(message, number):
+    import json, urllib2
+    from urllib import quote_plus
+
+    dd = "&".join(["-d", "number=%s" % number, "-d", "message=%s" % message])
+    req = urllib2.urlopen("http://textbelt.com/text", dd)
+    data = json.loads(req.read())
+
+    if not data["success"]:
+        print "Error sending text:", data["message"]
+    return data["success"]
+
+#----------------------------------------------------------------------#
+# Test
+
+def main():
+
+    # Text number
+    status = send_text("Here is a text!", sys.argv[1])
+    if (status):
+        print "Success!"
+    else:
+        print "No!"
+
+#----------------------------------------------------------------------#
+if __name__ == "__main__":
+    main()
